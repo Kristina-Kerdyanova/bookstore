8cf232410206c0febdf9c21576a0e6d8e2e80bcb
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-05-27 01:06:46 +0300

    fix(app): add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
-import { useEffect, useState } from "react";
-import { BookList } from "./components/BookList/BookList";
-import { IBook } from "./types/types";
+import { BrowserRouter } from 'react-router-dom'
+import { AppRouter } from './router/AppRouter'
 
 export const App = () => {
-  const [books, setBooks] = useState<IBook[]>([]);
-
-  useEffect(() => {
-    fetch("https://api.itbook.store/1.0/new")
-      .then((res) => res.json())
-      .then((data) => {
-        setBooks(data.books);
-      });
-  }, []);
-
   return (
-    <div>
-      <h1>Books</h1>
-      <BookList books={books} />
-    </div>
-  );
-};
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
+  )
+}
