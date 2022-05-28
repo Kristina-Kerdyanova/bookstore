ed86fb149aae71d17306990b96118d0a35d89f4d
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-05-29 01:18:55 +0300

    fix(pages): fix Acoount, Register

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,11 +1,13 @@
+import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import { routers } from '../routes/routers';
+import { RootStore } from '../store/store';
 
 export const Account = () => {
-    const isAuth = false;
+    const { isAuth, email } = useSelector(({user}: RootStore) => user);
 
     if (isAuth) {
-        return <div>Account</div>
+        return <div>{email}</div>
     }
     return (
         <Navigate to={routers.SIGN_UP} />
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,23 +1,34 @@
-import React, { FormEvent } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { routers } from '../routes/routers'
+import { useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
+import { routers } from '../routes/routers';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { useDispatch } from 'react-redux';
+import { setUser } from '../store/slices/userReducer';
 
 export const Register = () => {
     const navigate = useNavigate();
+    const dispath = useDispatch();
+    const { register, handleSubmit } = useForm();
 
-    const onSubmit = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
 
-        navigate(routers.ACCOUNT);
+    const onSubmit = (data: any) => {
+        const auth = getAuth();
+
+        createUserWithEmailAndPassword(auth, data.email, data.password)
+            .then((userCredential) => {
+                dispath(setUser(userCredential.user.email));
+                navigate(routers.ACCOUNT);
+            })
+            .catch(console.error);
     };
 
 
     return (
         <div>
             <h1>Register</h1>
-            <form onSubmit={onSubmit}>
-                <input type="text" />
-                <input type="text" />
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input type="email" {...register('email')} />
+                <input type="password" {...register('password')} />
                 <button type='submit'>Send</button>
             </form>
             <Link to={routers.SIGN_IN}>Have account</Link>
