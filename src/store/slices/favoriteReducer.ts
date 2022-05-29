85d0879be740a662c00c17187b987c96703ed2fd
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-05-30 01:56:33 +0300

    feat(slices): add favoriteReducer

---
+++ src/store/slices/favoriteReducer.ts
@@ -0,0 +1,29 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { IBook, IFavorite } from "./types";
+
+const initialState: IFavorite = {
+  sortMode: "asc",
+  favorites: [],
+};
+
+const favoriteSlice = createSlice({
+  name: "favorite",
+  initialState,
+  reducers: {
+    addFavorite: (state, { payload }: PayloadAction<IBook>) => {
+      state.favorites = [
+        { ...payload },
+        ...state.favorites.filter((item) => item.isbn13 !== payload.isbn13),
+      ];
+    },
+    removeFavorite: (state, {payload}:  PayloadAction<IBook>) => {
+        state.favorites = state.favorites.filter(
+            item => item.isbn13 !== payload.isbn13
+        );
+    },
+  },
+});
+
+export default favoriteSlice.reducer;
+
+export const { addFavorite, removeFavorite } = favoriteSlice.actions;
