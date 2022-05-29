5dcadb375a434cb67b8b38aa1067d125ec837734
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-05-30 01:46:37 +0300

    feat(pages): add Favorites

---
+++ src/pages/Favorite.tsx
@@ -0,0 +1,38 @@
+import { useDispatch } from 'react-redux';
+import { useAppSelector } from '../store/hooks/hooks';
+import { getFavorite } from '../store/selectors/favoriteSelector';
+import { removeFavorite } from '../store/slices/favoriteReducer';
+import { IBook } from '../store/slices/types';
+
+
+export const Favorite = () => {
+  const { sortMode, favorites } = useAppSelector(getFavorite);
+  const dispatch = useDispatch();
+
+  const handleRemoveFavorite = (book: IBook) => {
+    dispatch(removeFavorite(book));
+
+  }
+
+  return (
+    <><header>
+      <h1>YOUR FAVORITES</h1>
+      <p>{favorites.length}</p>
+    </header>
+      <ul>
+        {favorites.map(book => (
+          <li key={book.isbn13}>
+            <img src={book.image} alt={book.title} />
+            <h2>{book.title}</h2>
+            <p> {book.subtitle}</p>
+            <p>{book.price}</p>
+            <button
+              onClick={() => handleRemoveFavorite(book)}
+            >
+              ❤
+            </button>
+          </li>
+        ))}
+      </ul></>
+  );
+};
