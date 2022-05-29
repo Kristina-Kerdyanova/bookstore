48db874a0120550ad58bca2321012467fc4cbb20
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-05-30 01:50:54 +0300

    feat(fselectors): add booksselectors

---
+++ src/store/selectors/booksSelector.ts
@@ -0,0 +1,6 @@
+import { RootState } from "../store";
+
+export const getNewBooks = (state: RootState) => state.newBooks;
+// export const getSearch = (state: RootState) => state.Search;
+// export const getdetailBooks = (state: RootState) => state.detailBooks;
+
