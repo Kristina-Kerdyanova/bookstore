8a49c55773c45c5b2c7e8b601e6158ff507c6131
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-05-30 01:56:15 +0300

    feat(slices): add cartReducer

---
+++ src/store/slices/cartReducer.ts
@@ -0,0 +1,27 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { IBook, ICart } from "./types";
+
+const initialState: ICart = {
+  buy: [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addBuy: (state, { payload }: PayloadAction<IBook>) => {
+        state.buy = [
+          { ...payload },
+          ...state.buy.filter((item) => item.isbn13 !== payload.isbn13),
+        ];
+      },
+      deleteBuy: (state, {payload}:  PayloadAction<IBook>) => {
+          state.buy = state.buy.filter(
+              item => item.isbn13 !== payload.isbn13
+          );
+      },
+  },
+});
+
+export default cartSlice.reducer;
+export const { addBuy, deleteBuy } = cartSlice.actions;
