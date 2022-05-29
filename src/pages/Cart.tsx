aaf94a3f7e023d5800e714b684f5c57ebf6c9535
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-05-30 01:45:45 +0300

    feat(pages): add Cart

---
+++ src/pages/Cart.tsx
@@ -0,0 +1,34 @@
+import { useDispatch } from 'react-redux';
+import { useAppSelector } from '../store/hooks/hooks';
+import { getCart } from '../store/selectors/cartSelector';
+import { deleteBuy } from '../store/slices/cartReducer';
+import { IBook } from '../types';
+
+
+export const Cart = () => {
+    const { buy } = useAppSelector(getCart);
+    const dispatch = useDispatch();
+
+    const handleDeleteBuy = (book: IBook) => {
+        dispatch(deleteBuy(book));
+    };
+
+    return (
+        <><header>
+            <h1>YOUR CART</h1>
+            {/* <button>Back</button> */}
+            <p>{buy.length}</p>
+        </header>
+            <ul>
+                {buy.map(book => (
+                    <li key={book.isbn13}>
+                        <img src={book.image} alt={book.title} />
+                        <h2>{book.title}</h2>
+                        <p> {book.subtitle}</p>
+                        <p>{book.price}</p>
+                        <button onClick={() => handleDeleteBuy(book)}>x</button>
+                    </li>
+                ))}
+            </ul></>
+    );
+};
