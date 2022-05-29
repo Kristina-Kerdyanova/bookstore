afdfbfbd5cbce9ffd18d78988fd8d3145a5dad3c
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-05-30 01:54:15 +0300

    fix(components): fix MainTemplate

--- src/components/MainTemplate/MainTemplate.tsx
+++ src/components/MainTemplate/MainTemplate.tsx
@@ -1,21 +1,18 @@
-import { Link, Outlet } from "react-router-dom";
-import styled from "styled-components";
+import { Outlet } from "react-router-dom";
+import { StyledLink } from "./styles";
 
 export const MainTemplate = () => {
   return (
     <div>
       <nav>
-        <StyledLink to="/">Home</StyledLink>
-        <StyledLink to="/search">Search</StyledLink>
-        <StyledLink to="/favorite">Favorite</StyledLink>
-        <StyledLink to="/cart">Cart</StyledLink>
-        <StyledLink to="/account">Account</StyledLink>
+        <StyledLink to={"/"}>New Books</StyledLink>
+        <StyledLink to={"/search"}>Search</StyledLink>
+        <StyledLink to={"/favorite"}>Favorite</StyledLink>
+        <StyledLink to={"/cart"}>Cart</StyledLink>
+        <StyledLink to={"/account"}>Account</StyledLink>
+	  <Outlet />
       </nav>
-      <Outlet />
     </div>
   );
 };
 
-const StyledLink = styled(Link)`
-margin: 2px;
-`;
