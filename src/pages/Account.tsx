ef92b2184f939108d6bfe882689550a023a638b6
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-05-30 01:45:12 +0300

    fix(pages): fix Account

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,15 +1,15 @@
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import { routers } from '../routes/routers';
-import { RootStore } from '../store/store';
+import { RootState } from '../store/store';
 
 export const Account = () => {
-    const { isAuth, email } = useSelector(({user}: RootStore) => user);
+    const { isAuth, email } = useSelector(({user}: RootState) => user);
 
     if (isAuth) {
         return <div>{email}</div>
     }
     return (
-        <Navigate to={routers.SIGN_UP} />
+        <Navigate to={routers.SIGN_IN} />
     )
 }
