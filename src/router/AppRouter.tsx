5face0387fb92a441ba7bfbec7c9a2b26c243243
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-05-30 01:49:01 +0300

    fix(router): fix AppRouter

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { MainTemplate } from "../components/MainTemplate/MainTemplate";
 import { Account } from "../pages/Account";
+import { Cart } from "../pages/Cart";
 import { DetailsBook } from "../pages/DetailsBook";
 import { Login } from "../pages/Login";
 import { NewBooks } from "../pages/NewBooks";
@@ -21,15 +22,13 @@ export const AppRouter = () => {
           element={<SearchBooksResults />}
         />
         <Route path="books/:id" element={<DetailsBook />} />
+        <Route path='/cart' element={<Cart />} />
       </Route>
-      <Route path="account" element={<Account />} />
 
       <Route path={routers.ACCOUNT} element={<Account />} />
       <Route path={routers.SIGN_IN} element={<Login />} />
       <Route path={routers.SIGN_UP} element={<Register />} />
       <Route path={routers.NOT_FOUND} element={<NotFound />} />
-
-
 
     </Routes >
   );
