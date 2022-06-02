fe267d3015fe6bd65dfbfd424a29fa6b91c32f57
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-06-03 02:15:03 +0300

    fix(store): fix store

---
+++ src/store/store.tsx
@@ -0,0 +1,22 @@
+import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "./slices/cartReducer";
+import favoriteReducer from "./slices/favoriteReducer";
+import newBooksReducer from "./slices/newBooksReducer";
+import searchReducer from "./slices/searchReducer";
+import userReducer from "./slices/userReducer";
+
+
+
+export const store = configureStore({
+    reducer: {
+        newBooks: newBooksReducer,
+        search: searchReducer,
+        // detailsBook: detailsBookReducer,
+        favorite: favoriteReducer,
+        cart: cartReducer,
+        user: userReducer,
+    },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
