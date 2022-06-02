94c2e9cc452dee1d37a67ce308016b3dc229ca2f
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-06-03 02:22:53 +0300

    fix(slices): fix newBooksReducer

--- src/store/slices/newBooksReducer.ts
+++ src/store/slices/newBooksReducer.ts
@@ -1,36 +1,40 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { IBook } from "./types";
-const initialState: IBook[] = [
-  {
-    title: "Designing Across Senses",
-    subtitle: "A Multimodal Approach to Product Design",
-    isbn13: "9781491954249",
-    price: "$27.59",
-    image: "https://itbook.store/img/books/9781491954249.png",
-    url: "https://itbook.store/books/9781491954249",
-  },
-  {
-    title: "Web Scraping with Python, 2nd Edition",
-    subtitle: "Collecting More Data from the Modern Web",
-    isbn13: "9781491985571",
-    price: "$33.99",
-    image: "https://itbook.store/img/books/9781491985571.png",
-    url: "https://itbook.store/books/9781491985571",
-  },
-  {
-    title: "Programming iOS 11",
-    subtitle: "Dive Deep into Views, View Controllers, and Frameworks",
-    isbn13: "9781491999226",
-    price: "$59.17",
-    image: "https://itbook.store/img/books/9781491999226.png",
-    url: "https://itbook.store/books/9781491999226",
-  },
-];
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { bookApi } from "../../services/bookService";
+import { INewBooks } from "../types";
+
+const initialState: INewBooks = {
+  total: "",
+  books: [],
+  error: null,
+  status: "idle",
+};
+
+export const fetchNewBooks = createAsyncThunk<INewBooks>(
+  "new/fetchNewBooks",
+  async () => {
+    return bookApi.getBooks();
+  }
+);
 
 const newBooksReducer = createSlice({
-  name: "books",
+  name: "newBooks",
   initialState,
   reducers: {},
+
+  extraReducers: (builder) => {
+    builder.addCase(fetchNewBooks.pending, (state) => {
+      state.status = "loading";
+      state.error = null;
+    });
+    builder.addCase(fetchNewBooks.fulfilled, (state, { payload }) => {
+      state.status = "success";
+      state.books = payload.books;
+    });
+    builder.addCase(fetchNewBooks.rejected, (state, action) => {
+      state.status = "error";
+      state.error = action.error;
+    });
+  },
 });
 
 export default newBooksReducer.reducer;
