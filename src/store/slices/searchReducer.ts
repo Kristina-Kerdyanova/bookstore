8098b652a34b49fd853307fdda4ac83d055dde93
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-06-03 02:23:12 +0300

    feat(slices): add searchReducer

---
+++ src/store/slices/searchReducer.ts
@@ -0,0 +1,41 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { bookApi } from "../../services/bookService";
+import { ISearchBooksApi } from "../types";
+
+const initialState: ISearchBooksApi = {
+  total: "",
+  page: "",
+  books: [],
+  error: null,
+  status: "idle",
+};
+
+export const fetchSearch = createAsyncThunk<ISearchBooksApi>(
+  "/search/fetchSearch",
+  async () => {
+    return bookApi.getBooksBySearch("", "");
+  }
+);
+
+const searchReducer = createSlice({
+  name: "search",
+  initialState,
+  reducers: {},
+
+  extraReducers: (builder) => {
+    builder.addCase(fetchSearch.pending, (state) => {
+      state.status = "loading";
+      state.error = null;
+    });
+    builder.addCase(fetchSearch.fulfilled, (state, { payload }) => {
+      state.status = "success";
+      state.books = payload.books;
+    });
+    builder.addCase(fetchSearch.rejected, (state, action) => {
+      state.status = "error";
+      state.error = action.error;
+    });
+  },
+});
+
+export default searchReducer.reducer;
