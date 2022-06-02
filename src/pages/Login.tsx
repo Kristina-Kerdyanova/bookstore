7f43deb395d32ebfbc10bd8c642525349e50b081
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-06-03 02:29:26 +0300

    fix(pages): fix Login, NewBooks

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { routers } from '../routes/routers';
+import { setUser } from '../store/slices/userReducer';
 
 export const Login = () => {
-    return (
-        <div>
-            Login
-        </div>
-    )
-}
+    const navigate = useNavigate();
+    const dispath = useDispatch();
+    const { register, handleSubmit } = useForm();
+
+
+    const onSubmit = (data: any) => {
+        const auth = getAuth();
+        
+        signInWithEmailAndPassword(auth, data.email, data.password)
+            .then((userCredential) => {
+                dispath(setUser(userCredential.user.email));
+                navigate(routers.SIGN_IN);
+            })
+            .catch(console.error);
+
+        return (
+            <div>
+                <h1>Login</h1>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <input type="email" {...register('email')} />
+                    <input type="password" {...register('password')} />
+                    <button type='submit'>Send</button>
+                </form>
+                <Link to={routers.SIGN_IN}>Have account</Link>
+            </div>
+        );
+    };
+};
+
--- src/pages/NewBooks.tsx
+++ src/pages/NewBooks.tsx
@@ -1,11 +1,18 @@
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../store/hooks/hooks";
-import { getNewBooks } from "../store/selectors/booksSelector";
+import { getNewBooks, getNewBooksError, getNewBooksStatus } from "../store/selectors/booksSelector";
 import { addBuy } from "../store/slices/cartReducer";
 import { addFavorite } from "../store/slices/favoriteReducer";
-import { IBook } from "../types";
+import { fetchNewBooks } from "../store/slices/newBooksReducer";
+import { IBook } from "../store/types";
+
 
 export const NewBooks = () => {
   const books = useAppSelector(getNewBooks);
+  const status = useAppSelector(getNewBooksStatus);
+  const error = useAppSelector(getNewBooksError);
+
   const dispatch = useAppDispatch();
 
   const handleFavorite = (book: IBook) => {
@@ -15,16 +22,40 @@ export const NewBooks = () => {
   const handleAddBuy = (book: IBook) => {
     dispatch(addBuy(book));
   };
+
+  useEffect(() => {
+    if (status === "idle") {
+      dispatch(fetchNewBooks());
+    }
+  }, [dispatch, status]);
+
+  if (status === 'loading') {
+    return <div>Loading...</div>
+  };
+
+  if (status === 'error') {
+    return <div>
+      <h1>WE HAVE SOME PROBLEMS</h1>
+      <h2>{error.message}</h2>
+      <i>{error.code}</i>
+    </div>
+  };
+
   return (
     <div>
-      <h1>NewBooks</h1>
+      <h1>New books</h1>
       <ul>
-        {books.map(book => (
-          <li key={book.isbn13}>
-            <img src={book.image} alt={book.title} />
-            <h2>{book.title}</h2>
+        {books.map((book: IBook) => (
+          <li
+            key={book.isbn13}>
+            <Link to={`/books/${book.isbn13}`}>
+              <img src={book.image} alt={book.title} />
+              <h2>{book.title}</h2>
+            </Link>
             <p> {book.subtitle}</p>
             <p>{book.price}</p>
+
+
             <button
               onClick={() => handleFavorite(book)}
             >
@@ -42,3 +73,4 @@ export const NewBooks = () => {
     </div>
   );
 };
+
