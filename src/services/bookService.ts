2c24963a4a41a911b2a2c89baf4d3b1880d081a1
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-06-03 02:20:02 +0300

    fix(services): fix bookService

--- src/services/bookService.ts
+++ src/services/bookService.ts
@@ -1,16 +1,15 @@
 import axios from "axios";
-import { IBookDetailsApi, INewBooksApi, ISearchBooksApi } from "../types";
+import { IBookDetailsApi, INewBooks, ISearchBooksApi } from "../store/types";
 
 class BookService {
-  private readonly API_URL = "https://api.itbook.store/1.0/";
-
+  private readonly API_URL = "https://api.itbook.store/1.0";
   private api = axios.create({
-    baseURL: this.API_URL,
+      baseURL: this.API_URL,
   });
 
-  public async getNewBooks(): Promise<INewBooksApi> {
-    const { data } = await this.api.get<INewBooksApi>("/new");
-    return data;
+  public async getBooks(): Promise<INewBooks> {
+      const {data} = await this.api.get<INewBooks>('new');
+      return data;
   }
 
   public async getBookDetails(isbn: string): Promise<IBookDetailsApi> {
@@ -28,5 +27,8 @@ class BookService {
     return data;
   }
 }
+
+
+
 
 export const bookApi = new BookService();
