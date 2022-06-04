67edaf023984a90b5fc01fbf318e1307f650ac15
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-06-05 01:49:29 +0300

    fix(other): fix firebase

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,14 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyByrQ_3caW-MlXxJCXiVkupYpm6vN_IiBU",
-  authDomain: "bookstore-2726c.firebaseapp.com",
-  projectId: "bookstore-2726c",
-  storageBucket: "bookstore-2726c.appspot.com",
-  messagingSenderId: "591326159905",
-  appId: "1:591326159905:web:b8c819f80099c2b1c7af60"
+  apiKey: "AIzaSyCGuZVh395XSOwbQ6atsxBZcbQ4x5b25gs",
+  authDomain: "book-c3ddf.firebaseapp.com",
+  projectId: "book-c3ddf",
+  storageBucket: "book-c3ddf.appspot.com",
+  messagingSenderId: "494290447713",
+  appId: "1:494290447713:web:eecc429b576d92ffc6e920"
 };
 
 // Initialize Firebase
+// eslint-disable-next-line no-unused-vars
 const app = initializeApp(firebaseConfig);
