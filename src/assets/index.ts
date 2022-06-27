a44a5221a4eeaf478ba587e42576e31c60618df0
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-06-27 20:23:33 +0500

    fix(assets): fix assets

---
+++ src/assets/index.ts
@@ -0,0 +1,11 @@
+import { ReactComponent as Logo } from "./logo.svg";
+import { ReactComponent as Like} from "./like.svg";
+import { ReactComponent as Cart } from "./cart.svg";
+import { ReactComponent as User } from "./user.svg";
+import { ReactComponent as Search } from "./search.svg";
+import { ReactComponent as Back } from "./back.svg";
+import { ReactComponent as Cancel } from "./cancel.svg";
+import { ReactComponent as RedLike } from "./red_like.svg";
+import { ReactComponent as BlackStar } from "./black_star.svg";
+
+export { Logo, Like, Cart, User, Search, Back, Cancel, RedLike, BlackStar };
