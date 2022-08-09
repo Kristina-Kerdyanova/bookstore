9026aa083e2715bd92e6cdb211e7d8fa1c8f58ca
kkerdyanova <kristina.kerdyanova@gmail.com>
2022-08-09 18:29:17 +0500

    fix(page): modification account

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,16 +1,32 @@
-import { useSelector } from 'react-redux';
-import {  Navigate } from 'react-router-dom';
-import { routers } from '../routes/routers';
-import { RootState } from '../store/store';
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import { StyledLink } from "../components/Header/styled";
+import { routers } from "../routes/routers";
+import { useAppSelector } from "../store/hooks/hooks";
+import { getCart } from "../store/selectors/cartSelector";
+import { getFavorite } from "../store/selectors/favoriteSelector";
+import { RootState } from "../store/store";
+import { StyledSubtitle, StyledTitle } from "../styles";
 
 export const Account = () => {
-    const { isAuth, email } = useSelector(({ user }: RootState) => user);
+  const { isAuth } = useSelector(({ user }: RootState) => user);
+  const { favorites } = useAppSelector(getFavorite);
+  const { book } = useAppSelector(getCart);
 
-    if (isAuth) {
-        return <div>{email}</div>
-    }
-    
+
+  if (isAuth) {
     return (
-        <Navigate to={routers.SIGN_UP} />
-    )
-}
+      <>
+        <StyledTitle>Welcome to BOOKSTORE!</StyledTitle>
+        <StyledLink to={`/favorite`}>
+          <StyledSubtitle>YOUR FAVORITES BOOKS: {favorites.length}</StyledSubtitle>
+        </StyledLink>
+        <StyledLink to={`/cart`}>
+          <StyledSubtitle>YOUR CART: {book.length}</StyledSubtitle>
+        </StyledLink>
+      </>
+    );
+  }
+
+  return <Navigate to={routers.SIGN_UP} />;
+};
